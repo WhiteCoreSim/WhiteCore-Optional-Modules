941b705260f4bf79c6f5ea6963ec1879527a9be1
Fly Man <fly.man.opensim@gmail.com>
2014-09-24 02:19:56 +0200

    DefaultXMLInventoryAssetLoader
    * Adding in the "lost" XML Loader from Aurora.Optional

---
+++ DefaultXMLInventoryAssetLoader/Modules/DefaultAssetXMLLoader.cs
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) Contributors, http://whitecore-sim.org/ and http://aurora-sim.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Aurora-Sim Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using Nini.Config;
+using OpenMetaverse;
+using WhiteCore.Framework;
+using WhiteCore.Framework.Services;
+using WhiteCore.Framework.Services.ClassHelpers.Assets;
+using WhiteCore.Framework.Modules;
+using WhiteCore.Framework.ConsoleFramework;
+
+/// <summary>
+/// Loads assets from the file system location.  Not yet a plug-in, though it should be.
+/// </summary>
+namespace WhiteCore.DefaultLibraryLoaders
+{
+    public class DefaultAssetXMLLoader : IDefaultLibraryLoader
+    {
+        protected static readonly UUID LIBRARY_OWNER_ID = new UUID("11111111-1111-0000-0000-000100bba000");
+        protected ILibraryService m_service;
+
+        protected AssetBase CreateAsset(string assetIdStr, string name, string path, AssetType type)
+        {
+            AssetBase asset = new AssetBase(new UUID(assetIdStr), name, type, m_service.LibraryOwner);
+
+            if (!String.IsNullOrEmpty(path))
+            {
+                //MainConsole.Instance.InfoFormat("[ASSETS]: Loading: [{0}][{1}]", name, path);
+
+                LoadAsset(asset, path);
+            }
+            else
+            {
+                MainConsole.Instance.InfoFormat("[AssetsXMLLoader]: Instantiated: [{0}]", name);
+            }
+
+            return asset;
+        }
+
+        protected static void LoadAsset(AssetBase info, string path)
+        {
+            //            bool image =
+            //               (info.Type == (sbyte)AssetType.Texture ||
+            //                info.Type == (sbyte)AssetType.TextureTGA ||
+            //                info.Type == (sbyte)AssetType.ImageJPEG ||
+            //                info.Type == (sbyte)AssetType.ImageTGA);
+
+            FileInfo fInfo = new FileInfo(path);
+            long numBytes = fInfo.Length;
+            if (fInfo.Exists)
+            {
+                FileStream fStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                byte[] idata = new byte[numBytes];
+                BinaryReader br = new BinaryReader(fStream);
+                idata = br.ReadBytes((int)numBytes);
+                br.Close();
+                fStream.Close();
+                info.Data = idata;
+                //info.loaded=true;
+            }
+            else
+            {
+                MainConsole.Instance.ErrorFormat("[AssetsXMLLoader]: file: [{0}] not found !", path);
+            }
+        }
+
+        protected void ForEachDefaultXmlAsset(string assetSetFilename, Action<AssetBase> action)
+        {
+            List<AssetBase> assets = new List<AssetBase>();
+            if (File.Exists(assetSetFilename))
+            {
+                string assetSetPath = "ERROR";
+                string assetRootPath = "";
+                try
+                {
+                    DateTime start = DateTime.Now;
+                    XmlConfigSource source = new XmlConfigSource(assetSetFilename);
+                    assetRootPath = Path.GetFullPath(source.SavePath);
+                    assetRootPath = Path.GetDirectoryName(assetRootPath);
+
+                    for (int i = 0; i < source.Configs.Count; i++)
+                    {
+                        assetSetPath = source.Configs[i].GetString("file", String.Empty);
+
+                        LoadXmlAssetSet(Path.Combine(assetRootPath, assetSetPath), assets);
+                    }
+                    MainConsole.Instance.Warn((DateTime.Now - start).Milliseconds);
+                }
+                catch (XmlException e)
+                {
+                    MainConsole.Instance.ErrorFormat("[AssetsXMLLoader]: Error loading {0} : {1}", assetSetPath, e);
+                }
+            }
+            else
+            {
+                MainConsole.Instance.ErrorFormat("[AssetsXMLLoader]: Asset set control file {0} does not exist!  No assets loaded.", assetSetFilename);
+            }
+
+            DateTime start2 = DateTime.Now;
+            assets.ForEach(action);
+            MainConsole.Instance.Warn((DateTime.Now - start2).Milliseconds);
+        }
+
+        /// <summary>
+        /// Use the asset set information at path to load assets
+        /// </summary>
+        /// <param name="assetSetPath"></param>
+        /// <param name="assets"></param>
+        protected void LoadXmlAssetSet(string assetSetPath, List<AssetBase> assets)
+        {
+            //MainConsole.Instance.InfoFormat("[ASSETS]: Loading asset set {0}", assetSetPath);
+
+            if (File.Exists(assetSetPath))
+            {
+                try
+                {
+                    XmlConfigSource source = new XmlConfigSource(assetSetPath);
+                    String dir = Path.GetDirectoryName(assetSetPath);
+
+                    for (int i = 0; i < source.Configs.Count; i++)
+                    {
+                        string assetIdStr = source.Configs[i].GetString("assetID", UUID.Random().ToString());
+                        string name = source.Configs[i].GetString("name", String.Empty);
+                        AssetType type = (AssetType)source.Configs[i].GetInt("assetType", 0);
+                        string assetPath = Path.Combine(dir, source.Configs[i].GetString("fileName", String.Empty));
+
+                        AssetBase newAsset = CreateAsset(assetIdStr, name, assetPath, type);
+
+                        newAsset.Type = (int)type;
+                        assets.Add(newAsset);
+                    }
+                }
+                catch (XmlException e)
+                {
+                    MainConsole.Instance.ErrorFormat("[AssetsXMLLoader]: Error loading {0} : {1}", assetSetPath, e);
+                }
+            }
+            else
+            {
+                MainConsole.Instance.ErrorFormat("[AssetsXMLLoader]: Asset set file {0} does not exist!", assetSetPath);
+            }
+        }
+
+        #region IDefaultLibraryLoader Members
+
+        public void LoadLibrary(ILibraryService service, IConfigSource source, IRegistryCore registry)
+        {
+            m_service = service;
+
+            IConfig assetConfig = source.Configs["AssetsXMLLoader"];
+            if (assetConfig == null){
+                return;
+            }
+            string loaderArgs = assetConfig.GetString("AssetLoaderArgs",
+                        String.Empty);
+            bool assetLoaderEnabled = !assetConfig.GetBoolean("PreviouslyLoaded", false);
+
+            if (!assetLoaderEnabled)
+                return;
+
+            registry.RegisterModuleInterface<DefaultAssetXMLLoader>(this);
+
+            MainConsole.Instance.InfoFormat("[AssetsXMLLoader]: Loading default asset set from {0}", loaderArgs);
+            IAssetService assetService = registry.RequestModuleInterface<IAssetService>();
+            ForEachDefaultXmlAsset(loaderArgs,
+                    delegate(AssetBase a)
+                    {
+                        if (!assetLoaderEnabled && assetService.GetExists(a.IDString))
+                            return;
+                        assetService.Store(a);
+                    });
+        }
+
+        #endregion
+    }
+}
---
+++ DefaultXMLInventoryAssetLoader/Modules/DefaultInventoryXMLLoader.cs
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) Contributors, http://whitecore-sim.org/ and http://aurora-sim.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Aurora-Sim Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Xml;
+
+using WhiteCore.Framework;
+
+using Nini.Config;
+using OpenMetaverse;
+
+using WhiteCore.Modules;
+using WhiteCore.Framework.Services;
+using WhiteCore.Framework.Modules;
+using WhiteCore.Framework.Services.ClassHelpers.Inventory;
+using WhiteCore.Framework.ConsoleFramework;
+
+namespace WhiteCore.DefaultLibraryLoaders
+{
+    public class InventoryXMLLoader : IDefaultLibraryLoader
+    {
+        protected ILibraryService m_service;
+        protected IInventoryService m_inventoryService;
+
+        public void LoadLibrary(ILibraryService service, IConfigSource source, IRegistryCore registry)
+        {
+            m_service = service;
+            m_inventoryService = registry.RequestModuleInterface<IInventoryService>();
+
+            IConfig libConfig = source.Configs["InventoryXMLLoader"];
+            string pLibrariesLocation = Path.Combine("inventory", "Libraries.xml");
+            if (libConfig != null)
+            {
+                if (libConfig.GetBoolean("PreviouslyLoaded", false))
+                    return; //If it is loaded, don't reload
+                pLibrariesLocation = libConfig.GetString("DefaultLibrary", pLibrariesLocation);
+                LoadLibraries(pLibrariesLocation);
+            }
+        }
+
+        private InventoryItemBase CreateItem(UUID inventoryID, UUID assetID, string name, string description,
+                                            int assetType, int invType, UUID parentFolderID)
+        {
+            InventoryItemBase item = new InventoryItemBase();
+            item.Owner = m_service.LibraryOwner;
+            item.CreatorId = m_service.LibraryOwner.ToString();
+            item.ID = inventoryID;
+            item.AssetID = assetID;
+            item.Description = description;
+            item.Name = name;
+            item.AssetType = assetType;
+            item.InvType = invType;
+            item.Folder = parentFolderID;
+            item.BasePermissions = 0x7FFFFFFF;
+            item.EveryOnePermissions = 0x7FFFFFFF;
+            item.CurrentPermissions = 0x7FFFFFFF;
+            item.NextPermissions = 0x7FFFFFFF;
+            return item;
+        }
+
+        /// <summary>
+        /// Use the asset set information at path to load assets
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="assets"></param>
+        protected void LoadLibraries(string librariesControlPath)
+        {
+            MainConsole.Instance.InfoFormat("[InventoryXMLLoader]: Loading library control file {0}", librariesControlPath);
+            LoadFromFile(librariesControlPath, "Libraries control", ReadLibraryFromConfig);
+        }
+
+        /// <summary>
+        /// Read a library set from configuration
+        /// </summary>
+        /// <param name="config"></param>
+        protected void ReadLibraryFromConfig(IConfig config, string path)
+        {
+            string basePath = Path.GetDirectoryName(path);
+            string foldersPath
+                = Path.Combine(
+                    basePath, config.GetString("foldersFile", String.Empty));
+
+            LoadFromFile(foldersPath, "Library folders", ReadFolderFromConfig);
+
+            string itemsPath
+                = Path.Combine(
+                    basePath, config.GetString("itemsFile", String.Empty));
+
+            LoadFromFile(itemsPath, "Library items", ReadItemFromConfig);
+        }
+
+        /// <summary>
+        /// Read a library inventory folder from a loaded configuration
+        /// </summary>
+        /// <param name="source"></param>
+        private void ReadFolderFromConfig(IConfig config, string path)
+        {
+            InventoryFolderImpl folderInfo = new InventoryFolderImpl();
+
+            folderInfo.ID = new UUID(config.GetString("folderID", UUID.Random().ToString()));
+            folderInfo.Name = config.GetString("name", "unknown");
+            folderInfo.ParentID = new UUID (config.GetString ("parentFolderID", UUID.Zero.ToString ()));
+            folderInfo.Type = (short)config.GetInt("type", 8);
+
+            folderInfo.Owner = m_service.LibraryOwner;
+            folderInfo.Version = 1;
+
+            m_inventoryService.AddFolder(folderInfo);
+        }
+
+        /// <summary>
+        /// Read a library inventory item metadata from a loaded configuration
+        /// </summary>
+        /// <param name="source"></param>
+        private void ReadItemFromConfig(IConfig config, string path)
+        {
+            InventoryItemBase item = new InventoryItemBase();
+            item.Owner = m_service.LibraryOwner;
+            item.CreatorId = m_service.LibraryOwner.ToString();
+            item.ID = new UUID (config.GetString ("inventoryID", UUID.Random().ToString()));
+            item.AssetID = new UUID(config.GetString("assetID", item.ID.ToString()));
+            item.Folder = new UUID (config.GetString ("folderID", UUID.Zero.ToString ()));
+            item.Name = config.GetString("name", String.Empty);
+            item.Description = config.GetString("description", item.Name);
+            item.InvType = config.GetInt("inventoryType", 0);
+            item.AssetType = config.GetInt("assetType", item.InvType);
+            item.CurrentPermissions = (uint)config.GetLong("currentPermissions", 0x7FFFFFFF);
+            item.NextPermissions = (uint)config.GetLong("nextPermissions", 0x7FFFFFFF);
+            item.EveryOnePermissions = (uint)config.GetLong("everyonePermissions", 0x7FFFFFFF);
+            item.BasePermissions = (uint)config.GetLong("basePermissions", 0x7FFFFFFF);
+            item.Flags = (uint)config.GetInt("flags", 0);
+
+            m_inventoryService.AddItem(item);
+        }
+
+        private delegate void ConfigAction(IConfig config, string path);
+
+        /// <summary>
+        /// Load the given configuration at a path and perform an action on each Config contained within it
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="fileDescription"></param>
+        /// <param name="action"></param>
+        private void LoadFromFile(string path, string fileDescription, ConfigAction action)
+        {
+            if (File.Exists(path))
+            {
+                try
+                {
+                    XmlConfigSource source = new XmlConfigSource(path);
+
+                    for (int i = 0; i < source.Configs.Count; i++)
+                    {
+                        action(source.Configs[i], path);
+                    }
+                }
+                catch (XmlException e)
+                {
+                    MainConsole.Instance.ErrorFormat("[InventoryXMLLoader]: Error loading {0} : {1}", path, e);
+                }
+            }
+            else
+            {
+                MainConsole.Instance.ErrorFormat("[InventoryXMLLoader]: {0} file {1} does not exist!", fileDescription, path);
+            }
+        }
+    }
+}
